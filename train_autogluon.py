c20ffe29ebed6a81f9567930f4a20c1407ddbb30
Kartheek Akella <sukruthkartheek@gmail.com>
2024-10-04 00:22:26 +0530

    add cross validation

--- train_autogluon.py
+++ train_autogluon.py
@@ -1,38 +1,63 @@
 """
-This script trains an AutoGluon model for loan approval prediction.
+This script trains an AutoGluon model for loan approval prediction using 5-fold cross-validation.
 
 It performs the following main steps:
-1. Data preprocessing (including class balancing)
-2. Model training using AutoGluon's TabularPredictor
+1. Data preprocessing
+2. 5-fold cross-validation with AutoGluon's TabularPredictor
 3. Model evaluation
-4. Generating predictions for submission
+4. Generating out-of-fold (OOF) predictions for train and test sets
+5. Generating predictions for submission
 
 The script uses predefined constants for data paths, column names, and model parameters.
 Logging is implemented to capture all console output in a timestamped log file.
 """
 
+from collections import defaultdict
 from datetime import datetime
+from typing import List
 import pandas as pd
+import numpy as np
 from autogluon.tabular import TabularPredictor
 import logging
-from sklearn.utils import resample
+from sklearn.model_selection import StratifiedKFold
+from tqdm import tqdm
 
 # Constants
-DATA_DIR = '/Users/asvs/kartheek_hobby_projects/loan_approval_prediction/data'
+DATA_DIR = "/Users/asvs/kartheek_hobby_projects/loan_approval_prediction/data"
 
-CATEGORICAL_COLS = ['person_home_ownership', 'loan_intent', 'loan_grade', 'cb_person_default_on_file']
-NUMERICAL_COLS = ['person_age', 'person_income', 'person_emp_length', 'loan_amnt', 'loan_int_rate', 'loan_percent_income', 'cb_person_cred_hist_length']
-TARGET_COL = 'loan_status'
+CATEGORICAL_COLS = [
+    "person_home_ownership",
+    "loan_intent",
+    "loan_grade",
+    "cb_person_default_on_file",
+]
+NUMERICAL_COLS = [
+    "person_age",
+    "person_income",
+    "person_emp_length",
+    "loan_amnt",
+    "loan_int_rate",
+    "loan_percent_income",
+    "cb_person_cred_hist_length",
+]
+TARGET_COL = "loan_status"
 
-EVAL_METRIC = 'roc_auc'
-TIME_LIMIT = 10*60 # 10 minutes
-TIME_STAMP = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-EXPERIMENT_NAME = ''
+EVAL_METRIC = "roc_auc"
+TIME_LIMIT = 10 * 60  # 10 minutes
+TIME_STAMP = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+EXPERIMENT_NAME = "cv_5fold"
 LOG_FILE = f"logs/train_autogluon_{EXPERIMENT_NAME}_{TIME_STAMP}.log"
+N_FOLDS = 5
+RANDOM_STATE = 42
 
 # Set up logging
-logging.basicConfig(filename=LOG_FILE, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    filename=LOG_FILE,
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 logger = logging.getLogger()
+
 
 def data_preprocessing(df, test=False):
     """
@@ -44,52 +69,76 @@ def data_preprocessing(df, test=False):
 
     Returns:
         pandas.DataFrame: The preprocessed DataFrame.
-
-    This function performs the following preprocessing steps:
-    1. Creates a copy of the input DataFrame to avoid modifying the original.
-    2. Drops the 'id' column as it's not required for training or prediction.
-    3. Converts categorical columns to 'category' dtype for AutoGluon compatibility.
-    4. For training data (test=False), converts the target column to integer type.
     """
     df = df.copy()
-    df = df.drop(columns=['id']) # dropping id column as it is not required for training
-    df[CATEGORICAL_COLS] = df[CATEGORICAL_COLS].astype('category') # type casting categorical columns to category, necessary for AutoGluon
+    df = df.drop(
+        columns=["id"]
+    )  # dropping id column as it is not required for training
+    df[CATEGORICAL_COLS] = df[CATEGORICAL_COLS].astype(
+        "category"
+    )  # type casting categorical columns to category, necessary for AutoGluon
     if not test:
-        df[TARGET_COL] = df[TARGET_COL].astype('int') # type casting target column to int.
+        df[TARGET_COL] = df[TARGET_COL].astype(
+            "int"
+        )  # type casting target column to int.
 
     return df
 
-def train_model(df_train):
+
+def cross_validation(df_train, df_test) -> List[pd.DataFrame]:
     """
-    Train an AutoGluon TabularPredictor model on the preprocessed training data.
+    Perform 5-fold cross-validation on the training data and return the predictions for each fold.
 
     Args:
-        df_train (pandas.DataFrame): The preprocessed training DataFrame.
+        df_train (pandas.DataFrame): The training DataFrame.
+        df_test (pandas.DataFrame): The test DataFrame.
 
     Returns:
-        autogluon.tabular.TabularPredictor: The trained AutoGluon model.
-
-    This function initializes and trains an AutoGluon TabularPredictor with the following parameters:
-    - Label column: Specified by TARGET_COL constant
-    - Evaluation metric: Specified by EVAL_METRIC constant
-    - Time limit: Specified by TIME_LIMIT constant
-    - Verbosity: Set to 2 for detailed output
+        oof_train_preds (pandas.DataFrame): The out-of-fold predictions for the training data.
+        oof_test_preds (pandas.DataFrame): The out-of-fold predictions for the test data.
     """
-    predictor = TabularPredictor(label=TARGET_COL, eval_metric=EVAL_METRIC, log_to_file=True, log_file_path=LOG_FILE).fit(
-        df_train,
-        time_limit=TIME_LIMIT,
-        verbosity=2
-    )
+    oof_train_preds = defaultdict(list)
+    oof_test_preds = defaultdict(list)
+    kf = StratifiedKFold(n_splits=N_FOLDS, shuffle=True, random_state=RANDOM_STATE)
 
-    return predictor
+    for fold, (train_index, val_index) in tqdm(
+        enumerate(kf.split(df_train, df_train[TARGET_COL])),
+        total=N_FOLDS,
+        desc="Cross-validation",
+    ):
+        train_data = df_train.iloc[train_index]
+        val_data = df_train.iloc[val_index]
 
-def evaluate_predictor(predictor, df_train):
+        predictor = TabularPredictor(
+            label=TARGET_COL,
+            eval_metric=EVAL_METRIC,
+            log_to_file=True,
+            log_file_path=LOG_FILE,
+        ).fit(train_data, time_limit=TIME_LIMIT, verbosity=2)
+
+        # Evaluate the model on the validation data for this fold
+        val_metrics = predictor.evaluate(val_data)
+        val_roc_auc = val_metrics[EVAL_METRIC]
+        logger.info(f"Fold {fold} validation {EVAL_METRIC}: {val_roc_auc:.5f}")
+
+        # Evaluate predictor
+        train_preds = predictor.predict_proba(df_train)[1]
+        evaluate_predictor(predictor, df_train, train_preds)
+
+        oof_train_preds[f"fold_{fold}"] = train_preds
+        oof_test_preds[f"fold_{fold}"] = predictor.predict_proba(df_test)[1]
+
+    return pd.DataFrame(oof_train_preds), pd.DataFrame(oof_test_preds)
+
+
+def evaluate_predictor(predictor, df_train, train_preds):
     """
     Evaluate the trained AutoGluon model, log performance metrics, and save predictions.
 
     Args:
         predictor (autogluon.tabular.TabularPredictor): The trained AutoGluon model.
         df_train (pandas.DataFrame): The training DataFrame used to train the model.
+        train_preds (numpy.ndarray): The predictions on the training data.
 
     This function performs the following:
     1. Calculates and logs the model's performance metrics on the training data.
@@ -101,32 +150,26 @@ def evaluate_predictor(predictor, df_train):
     logger.info(f"Train metrics:\n {train_metrics}")
     logger.info(f"Feature importance:\n {feature_importance}")
 
-    # Save probability predictions on train dataset to a CSV file.
-    train_preds = predictor.predict_proba(df_train)
-    positive_class_preds = train_preds[1]
-    pred_df = df_train.copy()
-    pred_df['predicted_proba'] = positive_class_preds
-    pred_df.to_csv(f"predictions/train_preds_autogluon_{TIME_STAMP}.csv", index=False)
-    logger.info(f"Train predictions saved to predictions/train_preds_autogluon_{TIME_STAMP}.csv")
-
     # ROC_AUC score
     roc_auc = train_metrics[EVAL_METRIC]
     logger.info(f"ROC_AUC score: {roc_auc:.5f}")
 
     # Log the classification report with threshold 0.5
     from sklearn.metrics import classification_report
+
     y_true = df_train[TARGET_COL]
-    y_pred = (positive_class_preds > 0.5).astype(int)
+    y_pred = (train_preds > 0.5).astype(int)
     report = classification_report(y_true, y_pred)
     logger.info(f"Classification report:\n {report}")
 
-def save_submission_file(predictor, df_test, df_sub):
+
+def save_submission_file(oof_train_preds, oof_test_preds, df_sub):
     """
     Generate predictions for the test set and save them in a submission file.
 
     Args:
-        predictor (autogluon.tabular.TabularPredictor): The trained AutoGluon model.
-        df_test (pandas.DataFrame): The preprocessed test DataFrame.
+        oof_train_preds (pandas.DataFrame): The out-of-fold predictions for the training data.
+        oof_test_preds (pandas.DataFrame): The out-of-fold predictions for the test data.
         df_sub (pandas.DataFrame): The sample submission DataFrame with the correct format.
 
     This function performs the following steps:
@@ -135,11 +178,31 @@ def save_submission_file(predictor, df_test, df_sub):
     3. Adds the predictions to the submission DataFrame.
     4. Saves the submission DataFrame as a CSV file with a timestamped filename.
     """
-    df_test = df_test.copy()
     df_sub = df_sub.copy()
-    df_sub[TARGET_COL] = predictor.predict_proba(df_test)[1] # predicting the probability of the positive class, index 1 is for the positive class.
-    df_sub.to_csv(f"predictions/submission_autogluon_{TIME_STAMP}.csv", index=False)
-    logger.info(f"Submission file saved to predictions/submission_autogluon_{TIME_STAMP}.csv")
+    df_sub[TARGET_COL] = oof_test_preds.mean(
+        axis=1
+    )  # predicting the probability of the positive class, index 1 is for the positive class.
+    df_sub.to_csv(
+        f"predictions/submission_autogluon_{EXPERIMENT_NAME}_{TIME_STAMP}.csv",
+        index=False,
+    )
+    oof_train_preds.to_csv(
+        f"predictions/oof_train_preds_{EXPERIMENT_NAME}_{TIME_STAMP}.csv", index=False
+    )
+    oof_test_preds.to_csv(
+        f"predictions/oof_test_preds_{EXPERIMENT_NAME}_{TIME_STAMP}.csv", index=False
+    )
+    logger.info(f"Files saved:")
+    logger.info(
+        f"- Submission: predictions/submission_autogluon_{EXPERIMENT_NAME}_{TIME_STAMP}.csv"
+    )
+    logger.info(
+        f"- OOF train predictions: predictions/oof_train_preds_{EXPERIMENT_NAME}_{TIME_STAMP}.csv"
+    )
+    logger.info(
+        f"- OOF test predictions: predictions/oof_test_preds_{EXPERIMENT_NAME}_{TIME_STAMP}.csv"
+    )
+
 
 if __name__ == "__main__":
     """
@@ -156,17 +219,18 @@ if __name__ == "__main__":
     All console output is redirected to the log file specified by LOG_FILE constant.
     """
     try:
-        df_train = pd.read_csv(f'{DATA_DIR}/train.csv')
-        df_test  = pd.read_csv(f'{DATA_DIR}/test.csv')
-        df_sub = pd.read_csv(f'{DATA_DIR}/sample_submission.csv')
+        df_train = pd.read_csv(f"{DATA_DIR}/train.csv")
+        df_test = pd.read_csv(f"{DATA_DIR}/test.csv")
+        df_sub = pd.read_csv(f"{DATA_DIR}/sample_submission.csv")
 
-        logger.info(f"Original class distribution: {df_train['loan_status'].value_counts()}")
+        logger.info(
+            f"Original class distribution: {df_train['loan_status'].value_counts()}"
+        )
         mod_df_train = data_preprocessing(df_train)
         mod_df_test = data_preprocessing(df_test, test=True)
 
-        predictor = train_model(mod_df_train)
-        evaluate_predictor(predictor, df_train) # we need to pass the original df_train to the evaluate_predictor function, because the model has been trained on the downsampled df_train.
-        save_submission_file(predictor, mod_df_test, df_sub)
+        oof_train_preds, oof_test_preds = cross_validation(mod_df_train, mod_df_test)
+        save_submission_file(oof_train_preds, oof_test_preds, df_sub)
     except Exception as e:
         logger.exception("An error occurred during script execution:")
     finally:
